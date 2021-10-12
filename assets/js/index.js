89ee550f5069ee741bbcf1bd82c7eedcfb51e1ae
Andres <agomez.vg@gmail.com>
2021-10-11 21:54:01 -0300

    Se agrega buscador de productos funcionando ok

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,18 +17,26 @@ const getProducts = async () => {
 };
 
 const fillProductsInCards = async (products) => {
+  $(".product-card").remove();
   const contenedor = document.querySelector("#product-container");
 
-  products.forEach(
-    ({ name, url_image, price, discount }) =>
-      (contenedor.innerHTML += `
-    <div class="col mb-5">
+  products.forEach(({ name, url_image, price, discount }) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add(
+      "col-lg-3",
+      "col-md-4",
+      "col-sm-6",
+      "mb-4",
+      "product-card"
+    );
+    cardDiv.innerHTML += `
+   
                         <div class="card h-100">
                             <!-- Sale badge-->
                             ${
                               discount > 0
-                                ? `<div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">${discount}%</div>
-                                <h4 class="badge bg-danger text-white position-absolute" style="top: 0.5rem; left: 0.5rem">oferta</h4>
+                                ? `<h5><span class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem"> ${discount}%</span></h5>
+                                <h5> <span class="badge bg-danger text-white position-absolute" style="top: 0.5rem; left: 0.5rem"> oferta</span></h5>
                             <!-- Product image-->`
                                 : ""
                             }
@@ -45,6 +53,21 @@ const fillProductsInCards = async (products) => {
                                     
                                 </div>
                             </div>
-     `)
-  );
+     `;
+    contenedor.appendChild(cardDiv);
+  });
+};
+
+const searchProducts = async () => {
+  let searchInput = document.querySelector("#search-input").value;
+
+  try {
+    const { data } = await axios.get(
+      `http://localhost:3000/api/products/${searchInput}`
+    );
+    console.log(data);
+    await fillProductsInCards(data);
+  } catch (error) {
+    console.log("Error", error);
+  }
 };
