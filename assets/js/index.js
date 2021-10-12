ed56dcd503c81d952a3b3895419277cd6df9b353
Andres <agomez.vg@gmail.com>
2021-10-11 23:36:39 -0300

    Se agrega filtro por categoria y arreglo de las imagenes que no existen

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,6 +16,15 @@ const getProducts = async () => {
   }
 };
 
+const getAllProducts = async () => {
+  try {
+    const data = await getProducts();
+    await fillProductsInCards(data);
+  } catch (error) {
+    console.log("Error", error);
+  }
+};
+
 const fillProductsInCards = async (products) => {
   $(".product-card").remove();
   const contenedor = document.querySelector("#product-container");
@@ -41,7 +50,11 @@ const fillProductsInCards = async (products) => {
                                 : ""
                             }
                             
-                            <img class="card-img-top" src="${url_image}" alt="..." />
+                            <img class="card-img-top" src="${
+                              url_image == "" || url_image == null
+                                ? `https://www.cuestalibros.com/content/images/thumbs/default-image_550.png`
+                                : url_image
+                            }" alt="..." />
                             <!-- Product details-->
                             <div class="card-body p-4">
                                 <div class="text-center">
@@ -49,7 +62,14 @@ const fillProductsInCards = async (products) => {
                                     <h5 class="fw-bolder">${name}</h5>
                                     
                                     <!-- Product price-->
-                                    <span class="text-muted text-decoration-line-through">${price}</span>
+                                    ${
+                                      discount > 0
+                                        ? ` <span class="text-muted text-decoration-line-through">$${
+                                            ((100 - discount) * price) / 100
+                                          }</span>
+                                          <span class="ex-price">$${price}</span>`
+                                        : `<span class="text-muted text-decoration-line-through">$${price}</span>`
+                                    }
                                     
                                 </div>
                             </div>
@@ -67,6 +87,17 @@ const searchProducts = async () => {
     );
     console.log(data);
     await fillProductsInCards(data);
+  } catch (error) {
+    console.log("Error", error);
+  }
+};
+const getProductsByCategory = async (category) => {
+  try {
+    const {
+      data: [{ Products }],
+    } = await axios.get(`http://localhost:3000/api/categories/${category}`);
+
+    await fillProductsInCards(Products);
   } catch (error) {
     console.log("Error", error);
   }
