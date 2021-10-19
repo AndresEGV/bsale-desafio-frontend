94f52fe8f04a48dd50df9f76b8a84f6b009111b5
Andres <agomez.vg@gmail.com>
2021-10-19 19:44:44 -0300

    Comentarios agregados al código

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,7 @@
+//Url de la api para hacer las peticiones
 const api_url = "https://bsale-test-store.herokuapp.com";
+
+//Con el evento DOMContentLoaded, una vez que el HTML está completamente cargado y el árbol DOM está construido se ejecuta la función getProducts para traer la data y posteriormente renderizarla
 document.addEventListener("DOMContentLoaded", async () => {
   try {
     const products = await getProducts();
@@ -7,7 +10,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     console.log(error);
   }
 });
-
+//Función que permite traer todos los productos de la api con axios y el metodo get
 const getProducts = async () => {
   try {
     const { data } = await axios.get(`${api_url}/api/products`);
@@ -16,7 +19,7 @@ const getProducts = async () => {
     console.log("Error", error);
   }
 };
-
+//Función que permite traer todos los productos que se usa en la seccion del navbar
 const getAllProducts = async () => {
   try {
     const data = await getProducts();
@@ -25,12 +28,16 @@ const getAllProducts = async () => {
     console.log("Error", error);
   }
 };
-
+//Funcion que permite crear y llenar en el DOM cada card de los productos obtenidos de la api
 const fillProductsInCards = async (products) => {
+  //Método Jquery el cual se utiliza para remover elementos seleccionados en el DOM
+  //Cada vez que se llama a esta funcion,  se eliminan las cards para renderizarlas nuevamente
   $(".product-card").remove();
-  const contenedor = document.querySelector("#product-container");
 
+  const contenedor = document.querySelector("#product-container");
+  //Método para recorrer el arreglo de productos junto con sus propiedades para llenarlos en las cards
   products.forEach(({ name, url_image, price, discount }) => {
+    //Crear elemento div y asignarle clases de bootstrap
     const cardDiv = document.createElement("div");
     cardDiv.classList.add(
       "col-lg-3",
@@ -39,11 +46,13 @@ const fillProductsInCards = async (products) => {
       "mb-4",
       "product-card"
     );
+    //Construcción de la Card con la información del método ForEach
     cardDiv.innerHTML += `
    
                         <div class="card h-100">
                             <!-- Sale badge-->
                             ${
+                              //Operador ternario para cuando algun producto tenga descuento, muestre en la card un badge que está en oferta
                               discount > 0
                                 ? `<h5><span class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem"> ${discount}%</span></h5>
                                 <h5> <span class="badge bg-danger text-white position-absolute" style="top: 0.5rem; left: 0.5rem"> oferta</span></h5>
@@ -52,6 +61,7 @@ const fillProductsInCards = async (products) => {
                             }
                             
                             <img class="card-img-top" src="${
+                              //operador ternario para cuando un producto no tenga imagen, muestra una imagen dummy
                               url_image == "" || url_image == null
                                 ? `https://www.cuestalibros.com/content/images/thumbs/default-image_550.png`
                                 : url_image
@@ -63,7 +73,9 @@ const fillProductsInCards = async (products) => {
                                     <h5 class="fw-bolder">${name}</h5>
                                     
                                     <!-- Product price-->
+                                    
                                     ${
+                                      //operador ternario para hacer descuento en le precio de producto si asi lo aplica
                                       discount > 0
                                         ? ` <span class="text-muted text-decoration-line-through">$${
                                             ((100 - discount) * price) / 100
@@ -75,10 +87,11 @@ const fillProductsInCards = async (products) => {
                                 </div>
                             </div>
      `;
+    //Adjuntar elemento div que contienen las cards de los productos a su div padre
     contenedor.appendChild(cardDiv);
   });
 };
-
+// Función para buscar productos en la api. Opera con el evento onkeyup, significa que a medida que el usuario teclea (al soltar la tecla específicamente) la busqueda se va realizando al instante
 const searchProducts = async () => {
   let searchInput = document.querySelector("#search-input").value;
 
